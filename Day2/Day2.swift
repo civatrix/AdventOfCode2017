afa93ad9a34cfcc952afcc4aff3b268799f38931
Daniel <daniel.johns@robotsandpencils.com>
2023-03-03 15:54:09 -0800

    Day 2 Part 2

--- Day2/Day2.swift
+++ Day2/Day2.swift
@@ -11,7 +11,8 @@ final class Day2: Day {
     func run(input: String) -> String {
         return input.lines
             .map { $0.allDigits }
-            .map { $0.max()! - $0.min()! }
+            .map { $0.permutations(ofCount: 2).first { $0[0].isMultiple(of: $0[1]) || $0[1].isMultiple(of: $0[0]) }! }
+            .map { $0.max()! / $0.min()! }
             .sum
             .description
     }
