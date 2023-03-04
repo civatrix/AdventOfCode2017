f5188db414007448d5a6aac138e0d4be0b059e8b
Daniel <daniel.johns@robotsandpencils.com>
2023-03-03 16:22:15 -0800

    Day 3 Part 1

--- Day3/Day3.swift
+++ Day3/Day3.swift
@@ -9,6 +9,21 @@ import Foundation
 
 final class Day3: Day {
     func run(input: String) -> String {
+        let target = Int(input)!
+        
+        var ring = 1
+        while (ring * ring) < target {
+            ring += 2
+        }
+        
+        let corners = (0 ..< 4).map { (ring * ring) - ($0 * (ring - 1))}
+        for corner in corners {
+            let distance = abs(corner - target)
+            if distance <= (ring - 1) / 2 {
+                return (ring - 1 - distance).description
+            }
+        }
+        
         return ""
     }
 }
--- Day3/Day3Tests.swift
+++ Day3/Day3Tests.swift
@@ -13,7 +13,32 @@ final class Day3Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+1
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "0")
+    }
+    
+    func testDay2() throws {
+        let input =
+"""
+12
+"""
+        XCTAssertEqual(day.run(input: input), "3")
+    }
+    
+    func testDay3() throws {
+        let input =
+"""
+23
+"""
+        XCTAssertEqual(day.run(input: input), "2")
+    }
+    
+    func testDay4() throws {
+        let input =
+"""
+1024
+"""
+        XCTAssertEqual(day.run(input: input), "31")
     }
 }
