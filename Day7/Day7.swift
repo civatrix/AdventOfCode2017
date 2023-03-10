2ea208c9c7490f43ac91beab497589d4864d5e8d
Daniel <daniel.johns@robotsandpencils.com>
2023-03-09 18:01:26 -0800

    Day7 Part 1

--- Day7/Day7.swift
+++ Day7/Day7.swift
@@ -6,9 +6,31 @@
 //
 
 import Foundation
+import RegexBuilder
 
 final class Day7: Day {
     func run(input: String) -> String {
-        return ""
+        let name = Reference<Substring>()
+        let weight = Reference<Int>()
+        let regex = Regex {
+            Capture(as: name) { OneOrMore(.word) }
+            " ("
+            Capture(as: weight) { OneOrMore(.digit) } transform: { Int($0)! }
+            ")"
+            Optionally {
+                " -> "
+                Capture { OneOrMore(.any) } transform: { $0.split(separator: ", ") }
+            }
+        }
+        
+        var heads = Set<Substring>()
+        var tails = Set<Substring>()
+        for line in input.lines {
+            let match = line.wholeMatch(of: regex)!
+            heads.insert(match[name])
+            tails.formUnion(match.output.3 ?? [])
+        }
+        
+        return String(heads.subtracting(tails).first!)
     }
 }
