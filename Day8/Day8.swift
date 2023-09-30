24b6c0a14164bea4f090d8a5bafb727c3f113bdb
Daniel <daniel.johns@robotsandpencils.com>
2023-09-29 22:02:22 -0700

    Day 8 Part 1

--- Day8/Day8.swift
+++ Day8/Day8.swift
@@ -9,6 +9,37 @@ import Foundation
 
 final class Day8: Day {
     func run(input: String) -> String {
-        return ""
+        var registers = [Substring: Int]()
+        
+        for line in input.lines {
+            let words = line.split(separator: " ")
+            let comparator = words[5]
+            let lhs = words[4]
+            let rhs = Int(words[6])!
+            
+            let passed: Bool
+            switch comparator {
+            case "==":
+                passed = registers[lhs, default: 0] == rhs
+            case ">":
+                passed = registers[lhs, default: 0] > rhs
+            case ">=":
+                passed = registers[lhs, default: 0] >= rhs
+            case "<":
+                passed = registers[lhs, default: 0] < rhs
+            case "<=":
+                passed = registers[lhs, default: 0] <= rhs
+            case "!=":
+                passed = registers[lhs, default: 0] != rhs
+            default:
+                fatalError("Unrecognized comparator: \(comparator) in \(line)")
+            }
+            
+            guard passed else { continue }
+            let sign = (words[1] == "inc") ? 1 : -1
+            registers[words[0], default: 0] += sign * Int(words[2])!
+        }
+        
+        return registers.values.max()!.description
     }
 }
--- Day8/Day8Tests.swift
+++ Day8/Day8Tests.swift
@@ -13,7 +13,11 @@ final class Day8Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "1")
     }
 }
