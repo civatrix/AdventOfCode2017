6b05ceab15fdc1659c6b06dbac0f26010a543f7b
Daniel <daniel.johns@robotsandpencils.com>
2023-09-29 22:37:59 -0700

    Day 9 Part 1

--- Day9/Day9.swift
+++ Day9/Day9.swift
@@ -9,6 +9,35 @@ import Foundation
 
 final class Day9: Day {
     func run(input: String) -> String {
-        return ""
+        var score = 0
+        
+        var isGarbage = false
+        var skipNext = false
+        var depth = 0
+        for character in input {
+            if skipNext {
+                skipNext = false
+                continue
+            }
+            if isGarbage && (character != ">" && character != "!") {
+                continue
+            }
+            switch character {
+            case "{":
+                depth += 1
+            case "}":
+                score += depth
+                depth -= 1
+            case "<":
+                isGarbage = true
+            case ">":
+                isGarbage = false
+            case "!":
+                skipNext = true
+            default: continue
+            }
+        }
+        
+        return score.description
     }
 }
--- Day9/Day9Tests.swift
+++ Day9/Day9Tests.swift
@@ -10,10 +10,67 @@ import XCTest
 final class Day9Tests: XCTestCase {
     let day = Day9()
     
-    func testDay() throws {
+    func testDay1() throws {
         let input =
 """
+{}
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "1")
+    }
+    
+    func testDay2() throws {
+        let input =
+"""
+{{{}}}
+"""
+        XCTAssertEqual(day.run(input: input), "6")
+    }
+    
+    func testDay3() throws {
+        let input =
+"""
+{{},{}}
+"""
+        XCTAssertEqual(day.run(input: input), "5")
+    }
+    
+    func testDay4() throws {
+        let input =
+"""
+{{{},{},{{}}}}
+"""
+        XCTAssertEqual(day.run(input: input), "16")
+    }
+    
+    func testDay5() throws {
+        let input =
+"""
+{<a>,<a>,<a>,<a>}
+"""
+        XCTAssertEqual(day.run(input: input), "1")
+    }
+    
+    func testDay6() throws {
+        let input =
+"""
+{{<ab>},{<ab>},{<ab>},{<ab>}}
+"""
+        XCTAssertEqual(day.run(input: input), "9")
+    }
+    
+    func testDay7() throws {
+        let input =
+"""
+{{<!!>},{<!!>},{<!!>},{<!!>}}
+"""
+        XCTAssertEqual(day.run(input: input), "9")
+    }
+    
+    func testDay8() throws {
+        let input =
+"""
+{{<a!>},{<a!>},{<a!>},{<ab>}}
+"""
+        XCTAssertEqual(day.run(input: input), "3")
     }
 }
