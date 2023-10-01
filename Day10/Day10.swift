993cf23cbf63be21608f5721c58eb0246af67f51
Daniel <daniel.johns@robotsandpencils.com>
2023-09-30 22:13:01 -0700

    Day 10 Part 2

--- Day10/Day10.swift
+++ Day10/Day10.swift
@@ -11,22 +11,29 @@ final class Day10: Day {
     var loopSize = 256
     
     func run(input: String) -> String {
+        let codes = input.map { Int($0.asciiValue!) } + [17, 31, 73, 47, 23]
         var loop = (0 ..< loopSize).map { $0 }
         
         var current = 0
         var skip = 0
-        for length in input.allDigits {
-            loop.rotate(toStartAt: current)
-            let range = 0 ..< length
-            let reversed = loop[range].reversed()
-            loop[range] = ArraySlice(reversed)
-            loop.rotate(toStartAt: loopSize - current)
-            
-            current += (length + skip)
-            current %= loopSize
-            skip += 1
+        for _ in 0 ..< 64 {
+            for length in codes {
+                loop.rotate(toStartAt: current)
+                let range = 0 ..< length
+                let reversed = loop[range].reversed()
+                loop[range] = ArraySlice(reversed)
+                loop.rotate(toStartAt: loopSize - current)
+                
+                current += (length + skip)
+                current %= loopSize
+                skip += 1
+            }
         }
         
-        return (loop[0] * loop[1]).description
+        return loop
+            .map { UInt8(exactly: $0)! }
+            .chunks(ofCount: 16).map { chunk in chunk.reduce(0) { $0 ^ $1 } }
+            .map { String($0, radix: 16, uppercase: true) }
+            .joined()
     }
 }
