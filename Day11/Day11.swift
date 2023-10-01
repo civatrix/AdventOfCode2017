10a82ef9f502f293bc5873ce52653c673f24b697
Daniel <daniel.johns@robotsandpencils.com>
2023-09-30 22:42:53 -0700

    Day 11 Part 1

--- Day11/Day11.swift
+++ Day11/Day11.swift
@@ -9,6 +9,22 @@ import Foundation
 
 final class Day11: Day {
     func run(input: String) -> String {
-        return ""
+        let target: HexPoint = input
+            .split(separator: ",")
+            .map {
+                return switch $0 {
+                case "n": .n
+                case "ne": .ne
+                case "nw": .nw
+                case "s": .s
+                case "se": .se
+                case "sw": .sw
+                default:
+                    fatalError("Unknown direction \($0)")
+                }
+            }
+            .reduce(HexPoint.zero, +)
+        
+        return target.distance(to: .zero).description
     }
 }
--- Day11/Day11Tests.swift
+++ Day11/Day11Tests.swift
@@ -13,7 +13,32 @@ final class Day11Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+ne,ne,ne
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "3")
+    }
+    
+    func testDay2() throws {
+        let input =
+"""
+ne,ne,sw,sw
+"""
+        XCTAssertEqual(day.run(input: input), "0")
+    }
+    
+    func testDay3() throws {
+        let input =
+"""
+ne,ne,s,s
+"""
+        XCTAssertEqual(day.run(input: input), "2")
+    }
+    
+    func testDay4() throws {
+        let input =
+"""
+se,sw,se,sw,sw
+"""
+        XCTAssertEqual(day.run(input: input), "3")
     }
 }
