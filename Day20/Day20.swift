b56e8c487f2fb3b9de72d4d6108123bf8a08a998
Daniel <daniel.johns@robotsandpencils.com>
2023-10-07 15:38:37 -0700

    Day 20 Part 1

--- Day20/Day20.swift
+++ Day20/Day20.swift
@@ -7,8 +7,69 @@
 
 import Foundation
 
+class Particle: Comparable {
+    static func == (lhs: Particle, rhs: Particle) -> Bool {
+        (lhs.position == rhs.position) && (lhs.speed == rhs.speed) && (lhs.acceleration == rhs.acceleration)
+    }
+    
+    static func < (lhs: Particle, rhs: Particle) -> Bool {
+        (lhs.acceleration, lhs.speed, lhs.position) < (rhs.acceleration, rhs.speed, rhs.position)
+    }
+    
+    struct Vector: Comparable {
+        static func < (lhs: Vector, rhs: Vector) -> Bool {
+            (abs(lhs.x) + abs(lhs.y) + abs(lhs.z)) < (abs(rhs.x) + abs(rhs.y) + abs(rhs.z))
+        }
+        
+        static let zero = Vector(x: 0, y: 0, z: 0)
+        
+        public static func += (left: inout Vector, right: Vector) {
+            left = left + right
+        }
+        
+        public static func + (left: Vector, right: Vector) -> Vector {
+            Vector(x: left.x + right.x, y: left.y + right.y, z: left.z + right.z)
+        }
+        
+        public static func -= (left: inout Vector, right: Vector) {
+            left = left - right
+        }
+        
+        public static func - (left: Vector, right: Vector) -> Vector {
+            Vector(x: left.x - right.x, y: left.y - right.y, z: left.z - right.z)
+        }
+        
+        let x, y, z: Int
+        
+        var normalized: Vector {
+            Vector(x: x.clamped(to: -1 ... 1), y: y.clamped(to: -1 ... 1), z: z.clamped(to: -1 ... 1))
+        }
+    }
+    
+    init(_ numbers: [Int]) {
+        position = Vector(x: numbers[0], y: numbers[1], z: numbers[2])
+        speed = Vector(x: numbers[3], y: numbers[4], z: numbers[5])
+        acceleration = Vector(x: numbers[6], y: numbers[7], z: numbers[8])
+    }
+    
+    var position: Vector
+    var speed: Vector
+    let acceleration: Vector
+    var finished = false
+    
+    func update() {
+        speed += acceleration
+        position += speed
+        
+        finished = position.normalized == acceleration.normalized && position.normalized == speed.normalized
+    }
+}
+
 final class Day20: Day {
     func run(input: String) -> String {
-        return ""
+        let particles = input.lines.map { Particle($0.allDigits) }
+                
+        let closest = particles.min()!
+        return particles.firstIndex(of: closest)!.description
     }
 }
--- Day20/Day20Tests.swift
+++ Day20/Day20Tests.swift
@@ -13,7 +13,9 @@ final class Day20Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>
+p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "0")
     }
 }
